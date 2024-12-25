f6e718d700c987e135218873022f3d3dbb9428cf
Teklemya <Teklemya@mail.uc.edu>
2024-12-24 23:35:25 -0600

    added contact to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,26 @@ import Home from './Components/Home/Home'
 import Navbar from './Components/Navbar/Navbar';
 import Menu from './Components/Menu/Menu';
 import Order from './Components/Order/OrderPage';
+import ScrollToTop from './Components/ui/ScrollToTop';
 // import About from './About';
-// import Contact from './Contact';
+import Contact from './Components/ContactUs/ContactUs';
 import Catering from './Components/Catering/Catering';
 import './App.css';
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-         <Route path="/menu" element={<Menu />} />
         <Route path="/order" element={<Order />} />
+        <Route path="/menu" element={<Menu />} />
+        
         <Route path="/catering" element={<Catering />} />
-        {/*<Route path="/about" element={<About />} />
+        {/*<Route path="/about" element={<About />} />*/
         <Route path="/contact" element={<Contact />} />
-         */}
+         }
       </Routes>
     </Router>
   );
